7531c90d32e6ca525753d1410320146ff940a9e4
Priyanshu Varshney <priyanshuvarshney027@gmail.com>
2026-04-17 13:27:59 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,21 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+// app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://expense-tracker-nine-chi.vercel.app"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }
+}));
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/userRoutes"));
